4a9c988e8b56c23671eb25adb8ccb4ffd539ea78
James Browne <jamesabrowne64@gmail.com>
2025-09-08 10:58:41 +0100

    add .gitignore, add SQL to create client table, add python to run schema.sql

---
+++ scripts/setup_db.py
@@ -0,0 +1,29 @@
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="../.env")
+
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+
+def run_schema():
+    with open("../sql/schema.sql", "r") as f:
+        schema_sql = f.read()
+    
+    conn = psycopg2.connect(
+        dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST, port=DB_PORT
+    )
+
+    cur = conn.cursor()
+    cur.execute(schema_sql)
+    cur.close()
+    conn.close()
+    print("Database schema created")
+
+if __name__ == "__main__":
+    run_schema()
+
---
+++ sql/schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE clients (
+    client_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+)
